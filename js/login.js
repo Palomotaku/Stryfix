a9df3e565992b91a550b3c577cefe7fac655e00c
Palomotaku <francoitalo4@hotmail.com>
2023-09-12 23:34:24 -0500

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,2 +1,13 @@
 const Username = document.getElementById('Username')
 const Password = document.getElementById('Password')
+const input = document.getElementById('input')
+
+input.addEventListener('click', (e) => {
+    e.preventDefault()
+    const data ={
+        Username: Username.value,
+        Password: Password.value
+    }
+    console.log(data)
+})
+
