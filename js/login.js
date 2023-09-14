f67019a3b10e5cc9a85a37ee829c2bee164f88de
Palomotaku <francoitalo4@hotmail.com>
2023-09-14 16:28:47 -0500

    5

--- js/login.js
+++ js/login.js
@@ -1,9 +1,9 @@
 function Loguear(){
 
-    let Username = document.getElementById('Username').value;
+    let Email = document.getElementById('Correo').value;
     let Password = document.getElementById('Password').value;
     
-    if(Username=="Franco" && Password=="123")
+    if(Email=="Franco" && Password=="123")
     {
 
         window.location.href="inicial.html";
---
+++ js/registrar.js
@@ -0,0 +1,19 @@
+function Registrar(){
+    
+    const mysql = require("mysql2");
+    const conexion = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        database: "redsocial",
+        password: "root123"
+    });
+
+    const Nombre = document.getElementById('r_Name').value;
+    const Apellido = document.getElementById('r_LastName').value;
+    const Correo = document.getElementById('r_Correo').value;
+    const Password = document.getElementById('r_Pass').value;
+
+    const mysql2 = 'SELECT * FROM usuario where (Email, Nam, LNam, Pass, Pho, Ubi) VALUES(?,?,?,?,?,?);
+    const values = (Correo, Nombre, Apellido, Password,'','');
+    
+}  
